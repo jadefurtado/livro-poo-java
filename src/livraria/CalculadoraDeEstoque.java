bd74a3f2cd2b76bb243df52cc2013b1510c9e470
jadefurtado <jade.f.monteiro@gmail.com>
2023-06-05 17:09:35 -0300

    usando o for

--- src/livraria/CalculadoraDeEstoque.java
+++ src/livraria/CalculadoraDeEstoque.java
@@ -5,12 +5,8 @@ public class CalculadoraDeEstoque {
 	public static void main(String[] args) {
 		
 		double soma = 0;
-		int contador = 0;
-		
-		while (contador < 35) {
-			double valorDoLivro = 59.90;
-			soma += valorDoLivro;
-			contador ++;
+		for (int contador = 0; contador <35; contador ++) {
+			soma += 59.90;
 		}
 		
 		System.out.println("O total em estoque é " + soma);
