55dbeb3e8df2044c48383bf66d9b67e78a257f29
jadefurtado <jade.f.monteiro@gmail.com>
2023-06-05 16:57:43 -0300

    adicionando o while

--- src/livraria/CalculadoraDeEstoque.java
+++ src/livraria/CalculadoraDeEstoque.java
@@ -4,10 +4,14 @@ public class CalculadoraDeEstoque {
 
 	public static void main(String[] args) {
 		
-		double livroJava8 = 59.90;
-		double livroTDD = 59.90;
+		double soma = 0;
+		int contador = 0;
 		
-		double soma = livroJava8 + livroTDD;
+		while (contador < 35) {
+			double valorDoLivro = 59.90;
+			soma = soma + valorDoLivro;
+			contador = contador + 1;
+		}
 		
 		System.out.println("O total em estoque é " + soma);
 		
