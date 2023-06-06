58f1d1da497653256f577486101ddcad892e9571
jadefurtado <jade.f.monteiro@gmail.com>
2023-06-06 16:47:38 -0300

    adicionando método mostrarDetalhes

--- src/livraria/Livro.java
+++ src/livraria/Livro.java
@@ -5,5 +5,14 @@ public class Livro {
 	String descricao;
 	double valor;
 	String isbn;
+	
+	void mostrarDetalhes( ) {
+		String mensagem = "Mostrando detalhes do livro";
+		System.out.println(mensagem);
+		System.out.println(nome);
+		System.out.println(descricao);
+		System.out.println(valor);
+		System.out.println(isbn);
+	}
 
 }
