994658cbec02d4e3476748b3561a8a5cbb958ea2
jadefurtado <jade.f.monteiro@gmail.com>
2023-06-13 20:05:58 -0300

    regras de desconto atualizada

--- src/livraria/RegrasDeDesconto.java
+++ src/livraria/RegrasDeDesconto.java
@@ -4,16 +4,27 @@ public class RegrasDeDesconto {
 
 	public static void main(String[] args) {
 		
-		Livro livro = new Livro();
-		livro.valor = 59.90;
+		Autor autor = new Autor();
+		autor.setNome("Rodrigo Turini");
 		
-		System.out.println("Valor atual: " + livro.valor);
+		Livro livro = new Livro(autor);
+		livro.setValor(59.90);
 		
-		if (!livro.aplicaDescontoDe(0.1)) {
-			System.out.println("Desconto não pode ser maior do que 30%");
-			
-	} else {
-		System.out.println("Valor com desconto: " + livro.valor);
+		if(!livro.aplicaDescontoDe(0.3)) {
+			System.out.println("Desconto no livro não pode ser maior que 30%!");
+		} else {
+			System.out.println("Valor do livro com desconto: " + livro.getValor());
 		}
+		
+		Ebook ebook = new Ebook(autor);
+		ebook.setValor(29.90);
+		
+		if (!ebook.aplicaDescontoDe(0.3)) {
+			System.out.println("Desconto no ebook não pode ser maior do que 15%!");
+		} else {
+			System.out.println("Valor do ebook com desconto: " + ebook.getValor());
+		}
+		
 	}
+		
 }
