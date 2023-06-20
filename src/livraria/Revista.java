c868e7897ac1a4e9aeb1e5ae5e4ee9875070a789
jadefurtado <jade.f.monteiro@gmail.com>
2023-06-20 16:20:23 -0300

    criando classe revista e editora

---
+++ src/livraria/Revista.java
@@ -0,0 +1,36 @@
+package livraria;
+
+public class Revista {
+
+	private String nome;
+	private String descricao;
+	private double valor;
+	private Editora editora;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	public boolean aplicaDescontoDe(double porcentagem) {`
+		if (porcentagem > 0.1) {
+			return false;
+		}
+		double desconto = getValor() * porcentagem;
+		setValor(getValor() - desconto);
+		return true;
+	}
+}
