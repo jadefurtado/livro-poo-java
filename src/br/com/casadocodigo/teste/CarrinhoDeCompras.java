7e0ad841b5acc85f55efb157af20d3a72277a577
jadefurtado <jade.f.monteiro@gmail.com>
2023-07-03 21:30:48 -0300

    adicionando um array de produtos

--- src/br/com/casadocodigo/teste/CarrinhoDeCompras.java
+++ src/br/com/casadocodigo/teste/CarrinhoDeCompras.java
@@ -5,9 +5,13 @@ import br.com.casadocodigo.produtos.Produto;
 public class CarrinhoDeCompras {
 	
 	private double total;
+	private Produto [] produtos = new Produto[10];
+	private int contador = 0;
 	
 	public void adiciona(Produto produto) {
 		System.out.println("Adicionando: "+ produto);
+		this.produtos[contador] = produto;
+		contador ++;
 		total += produto.getValor();
 	}
 	
